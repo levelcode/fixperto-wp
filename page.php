49c0deb3439f7134d4b8218c5d96d6b9a420fee2
Gitea <gitea@fake.local>
2020-06-25 11:43:38 -0500

    Cambios

---
+++ page.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+	<?php the_post() ?>
+	<meta charset="utf-8">
+	<title><?php the_title() ?></title>
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<style>
+		body {
+			max-width: 800px;
+			margin: 20px auto;
+			font-family: "Arial", "Helvetica", sans-serif;
+		}
+
+		body > header {
+			margin-bottom: 50px;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<header>
+		<h1><?php the_title() ?></h1>
+	</header>
+	<main>
+		<?php the_content() ?>
+	</main>
+</body>
+</html>
